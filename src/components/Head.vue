dc695873e7e65ce5429355cc348cbba519aa8969
liucol <pearl-iooi@foxmail.com>
2018-06-11 10:02:05 +0800

    first commit

---
+++ src/components/Head.vue
@@ -0,0 +1,27 @@
+<template>
+        <div id="head">
+          <ul>
+            <li>首页</li>
+            <li>产品</li>
+            <li>个人中心</li>
+          </ul>
+        </div>
+</template>
+      
+<script>
+export default {
+  name: 'head'
+}
+</script>
+      
+<style>
+#head {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: red;
+  margin-top: 60px;
+}
+</style>
+      
